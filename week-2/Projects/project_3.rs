55582be9c4b7df6d7ec6297f55b1a42ec97bec53
Moses <115805769+MosesOnerhime@users.noreply.github.com>
2022-10-22 20:36:11 +0100

    Update project_3.rs

--- week-2/Projects/project_3.rs
+++ week-2/Projects/project_3.rs
@@ -4,8 +4,8 @@ fn main() {
 	let rate:f64 = 5.0;
 
 	// calculate the depreciation and amount after 3 years
-	let d:f64 = 1.0 - (r/100.0);
+	let d:f64 = 1.0 - (rate/100.0);
 	let d = d*d*d;
-	let Amount = p * d;
+	let Amount = principal * d;
 	println!("The value after 3 years is N{}", Amount); 
 }
