2fac5ed66a1f5f99c3f633954b3c412a90e1b60d
Moses <115805769+MosesOnerhime@users.noreply.github.com>
2022-10-24 11:14:21 +0100

    Update project_3.rs

--- week-2/Projects/project_3.rs
+++ week-2/Projects/project_3.rs
@@ -2,10 +2,10 @@ fn main() {
 	// allocate variables
 	let principal:f64 = 210000.0;
 	let rate:f64 = 5.0;
+	let n:f64 = 3.0;
 
 	// calculate the depreciation and amount after 3 years
-	let d:f64 = 1.0 - (rate/100.0);
-	let d = d*d*d;
-	let Amount = principal * d;
+	let d = 1.0 - (rate/100.0);
+	let Amount = principal * d.powf(n);
 	println!("The value after 3 years is N{}", Amount); 
 }
