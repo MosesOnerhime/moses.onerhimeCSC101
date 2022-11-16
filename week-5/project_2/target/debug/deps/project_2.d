8bfcd5349c499b623ab4a0d7be4abab8c4c74d75
Moses <115805769+MosesOnerhime@users.noreply.github.com>
2022-11-16 10:58:26 +0100

    Delete week-5/project_2/target directory

--- week-5/project_2/target/debug/deps/project_2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\HP\Desktop\moses.onerhimeCSC101\week-5\project_2\target\debug\deps\project_2.exe: src\main.rs
-
-C:\Users\HP\Desktop\moses.onerhimeCSC101\week-5\project_2\target\debug\deps\project_2.d: src\main.rs
-
-src\main.rs:
